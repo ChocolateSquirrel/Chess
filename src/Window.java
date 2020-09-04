8a1abb34d28f6468b6f66d7cdcd07aee6f247f47
ChocolateSquirrel <chocolate.squirrel@toto.com>
2020-09-04 17:33:29 +0200

    Movement for Knigth.

--- src/Window.java
+++ src/Window.java
@@ -37,7 +37,8 @@ public class Window extends JFrame {
 		Piece tower = new Tower("tower", 2, 4, "black");
 		Piece pawn1 = new WhitePawn("pawn", 4, 4, "white");
 		Piece pawn2 = new WhitePawn("pawn", 6, 4, "white");
-		for (Case case1 : tower.getAllowedCases(ChessboardPanel.getInstance().getGridCases())) {
+		Piece knight = new Knight("cavalier", 3, 5, "white");
+		for (Case case1 : knight.getAllowedCases(ChessboardPanel.getInstance().getGridCases())) {
 			ChessboardPanel.getInstance().getJPanel(case1).setBackground(Color.GREEN);
 		}	
 		
